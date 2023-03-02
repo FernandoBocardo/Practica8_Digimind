877dfcf59168f4c3902b203d82bc267b4311e285
Fernando <fernando.bocardo228476@potros.itson.edu.mx>
2023-03-02 15:13:07 -0700

    Practica 8.1

--- app/src/main/java/bocardo/fernando/mydigimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/bocardo/fernando/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,22 @@
 package bocardo.fernando.mydigimind.ui.dashboard
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import bocardo.fernando.mydigimind.R
+import java.text.SimpleDateFormat
+import java.util.*
 import bocardo.fernando.mydigimind.databinding.FragmentDashboardBinding
+import bocardo.fernando.mydigimind.ui.Task
+import bocardo.fernando.mydigimind.ui.home.HomeFragment
 
 class DashboardFragment : Fragment() {
 
@@ -28,9 +37,49 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val btn_time: Button = root.findViewById(R.id.btn_time)
+        btn_time.setOnClickListener{
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY,hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+
+        val done: Button = root.findViewById(R.id.done)
+        val et_titulo: EditText = root.findViewById(R.id.name)
+        val monday: CheckBox = root.findViewById(R.id.monday)
+        val tuesday: CheckBox = root.findViewById(R.id.tuesday)
+        val wednesday: CheckBox = root.findViewById(R.id.wednesday)
+        val thursday: CheckBox = root.findViewById(R.id.thursday)
+        val friday: CheckBox = root.findViewById(R.id.friday)
+        val saturday: CheckBox = root.findViewById(R.id.saturday)
+        val sunday: CheckBox = root.findViewById(R.id.sunday)
+        done.setOnClickListener{
+            var title = et_titulo.text.toString()
+            var time = btn_time.text.toString()
+            var days = ArrayList<String>()
+            if (monday.isChecked)
+                days.add("Monday")
+            if (tuesday.isChecked)
+                days.add("Tuesday")
+            if (wednesday.isChecked)
+                days.add("Wednesday")
+            if (thursday.isChecked)
+                days.add("Thursday")
+            if (friday.isChecked)
+                days.add("Friday")
+            if (saturday.isChecked)
+                days.add("Saturday")
+            if (sunday.isChecked)
+                days.add("Sunday")
+
+            var task = Task(title,days,time)
+            HomeFragment.tasks.add(task)
+            Toast.makeText(root.context, "New task added!", Toast.LENGTH_SHORT).show()
         }
         return root
     }
